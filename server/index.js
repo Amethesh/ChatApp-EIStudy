5680129a9b678c2533a7fd4d6d1be356ec9e4921
Amethesh <ametheshlgp@gmail.com>
2023-10-04 17:02:55 +0530

    Add server with socket.io

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    //To deal with cors errors
+    origin: "http://localhost:3000", //Client url (replace this during deployment)
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id); //Getting ids of connected users
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id); //Getting ids of users who disconnected
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING!");
+});
