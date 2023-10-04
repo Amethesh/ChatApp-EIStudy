1f517ce9b4eec363285ba31d59097613ed3efc82
Amethesh <ametheshlgp@gmail.com>
2023-10-04 20:17:28 +0530

    Fix: Style and classnames

--- client/src/Chat.tsx
+++ client/src/Chat.tsx
@@ -38,41 +38,16 @@ const Chat: React.FC<ChatProps> = ({ socket, username, room }) => {
       <div className="chat-header">
         <p>Live Chat</p>
       </div>
-      <div className="chat-body">
-        <ScrollToBottom className="message-container">
-          {messageList.map((messageContent) => {
-            return (
-              <div
-                className="message"
-                id={username === messageContent.author ? "you" : "other"}
-              >
-                <div>
-                  <div className="message-content">
-                    <p>{messageContent.message}</p>
-                  </div>
-                  <div className="message-meta">
-                    <p id="time">{messageContent.time}</p>
-                    <p id="author">{messageContent.author}</p>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </ScrollToBottom>
-      </div>
+      <div className="chat-body"></div>
       <div className="chat-footer">
         <input
           type="text"
-          value={currentMessage}
           placeholder="Hey..."
-          onChange={(event) => {
-            setCurrentMessage(event.target.value);
-          }}
-          onKeyPress={(event) => {
-            event.key === "Enter" && sendMessage();
+          onChange={(e) => {
+            setCurrentMessage(e.target.value);
           }}
         />
-        <button onClick={sendMessage}>&#9658;</button>
+        <button onClick={sendMessage}>Send</button>
       </div>
     </div>
   );
