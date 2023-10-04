8d4fe2555160b129a717554b0ecbcf6d78c41451
Amethesh <ametheshlgp@gmail.com>
2023-10-04 19:56:15 +0530

    Add style and classnames

--- client/src/Chat.tsx
+++ client/src/Chat.tsx
@@ -30,24 +30,49 @@ const Chat: React.FC<ChatProps> = ({ socket, username, room }) => {
     socket.on("receive_message", (data) => {
       console.log(data);
     });
-    return () => socket.removeListener("receive_message");
+    return () => socket.removeListener("receive_message"); //Cleanup function to avoid double message bug
   }, [socket]);
 
   return (
-    <div>
+    <div className="chat-window">
       <div className="chat-header">
         <p>Live Chat</p>
       </div>
-      <div className="chat-body"></div>
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
+          {messageList.map((messageContent) => {
+            return (
+              <div
+                className="message"
+                id={username === messageContent.author ? "you" : "other"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{messageContent.time}</p>
+                    <p id="author">{messageContent.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollToBottom>
+      </div>
       <div className="chat-footer">
         <input
           type="text"
+          value={currentMessage}
           placeholder="Hey..."
-          onChange={(e) => {
-            setCurrentMessage(e.target.value);
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+          onKeyPress={(event) => {
+            event.key === "Enter" && sendMessage();
           }}
         />
-        <button onClick={sendMessage}>Send</button>
+        <button onClick={sendMessage}>&#9658;</button>
       </div>
     </div>
   );
